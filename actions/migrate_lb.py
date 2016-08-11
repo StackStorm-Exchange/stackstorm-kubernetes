d031c4a7c0d1f9f880686fbb9edc9ce3935a5d8f
Andy Moore <andy@impulsed.net>
2016-08-11 14:31:24 +0200

    initial migrate lb script and metadata file

---
+++ actions/migrate_lb.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+import logging
+import os
+import json
+import boto3
+from datetime import datetime
+
+from st2actions.runners.pythonrunner import Action
+
+#variables required:
+# AWS_DEFAULT_REGION
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+#boto3.set_stream_logger('boto3.resources', logging.DEBUG)
+#boto3.set_stream_logger('botocore', logging.DEBUG)
+#boto3.set_stream_logger('boto3', logging.DEBUG)
+
+class ELBMigrate(Action):
+
+    def run(self, access_key, secret_key, environment, cluster):
+        """
+        Entry into the action script
+
+        :param str access_key: aws access key
+        :param str secret_key: aws secret key
+        :param str environment: bitesize cluster environment name
+        :param str cluster: cluster to migrate to live (a or b)
+
+        """
+
+        self.asgc = boto3.client('autoscaling', region_name='eu-west-1', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+
+        self.my_asgs = []
+        self.env = environment
+
+        #self.get_asgs()
+
+        if cluster not in ['a','b'] or cluster not in ['a', 'b']:
+            print "cluster must be a or b"
+            return 0
+
+        if cluster == "a":
+             other = "b"
+        else:
+             other = "a"
+
+        self.action(cluster, 'prelive', 'detach')
+        self.action(cluster, 'live', 'attach')
+        self.action(other, 'live', 'detach')
+        self.action(other, 'prelive', 'attach')
+
+    def get_elbs(self):
+        print json.dumps(self.elbc.describe_load_balancers()['LoadBalancerDescriptions'], sort_keys=True, indent=2, default=self.json_serial)
+        #for myelb in self.elbc.describe_load_balancers()['LoadBalancerDescriptions']:
+
+    def get_asgs(self):
+        for myasg in self.asgc.describe_auto_scaling_groups()['AutoScalingGroups']:
+            for tag in myasg['Tags']:
+                if tag['Key'] == "Environment" and tag['Value'] == self.env:
+                    self.my_asgs.append(tag['ResourceId'])
+
+    def action(self, stack, lb, action):
+        """ 
+        :param str lb: which load balancer (live|prelive)
+        :param str stack: a or b
+        :param str action: attach or detach
+        """
+
+        the_asg = "kubernetes-loadbalancer-%s-%s" % (self.env, stack)
+        the_lb  = "frontend-%s-%s" % (self.env, lb)
+
+        print "%s %s %s" % (action, the_asg, the_lb)
+
+        if action is "attach":
+            resp = self.asgc.attach_load_balancers(AutoScalingGroupName=the_asg, LoadBalancerNames=[ the_lb ])
+            code = resp['ResponseMetadata']['HTTPStatusCode']
+        if action is "detach":
+            asg_info = self.getinfo(the_asg)
+            if the_lb in asg_info['AutoScalingGroups'][0]['LoadBalancerNames']:
+                resp = self.asgc.detach_load_balancers(AutoScalingGroupName=the_asg, LoadBalancerNames=[ the_lb ])
+                code = resp['ResponseMetadata']['HTTPStatusCode']
+            else:
+                print "no lb attached"
+                code = 200
+
+        if code != 200:
+            print "failed resp: %s" % resp
+
+    def getlist(self, detail=0):
+        if detail == 0:
+            #print json.dumps(self.my_asgs, sort_keys=True, indent=2, default=self.json_serial)
+            return self.my_asgs
+        else: 
+            return self.asgc.describe_auto_scaling_groups(AutoScalingGroupNames=self.my_asgs)
+    
+    def getinfo(self, asg):
+        return self.asgc.describe_auto_scaling_groups(AutoScalingGroupNames=[asg])
+
+
+    def json_serial(self, obj):
+        """JSON serializer for objects not serializable by default json code"""
+
+        if isinstance(obj, datetime):
+            serial = obj.isoformat()
+            return serial
+        raise TypeError("Type not serializable")
+

630c4d1563de7693c6ac37ad7318f2f18a1bd03c
Martin Devlin <martin.devlin@pearson.com>
2016-11-02 12:25:53 +0000

    Add secret replace and delete actions

---
+++ actions/secret_delete.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import json
+import base64
+from st2actions.runners.pythonrunner import Action
+from lib import k8s
+from datetime import datetime
+
+deleteoptions = {}
+
+class SecretDelete(Action):
+
+    def run(self, ns, name):
+
+        k8suser = self.config.get('user')
+        k8spass = self.config.get('password')
+        k8surl  = self.config.get('kubernetes_api_url')
+
+        self.k8s = k8s.K8sClient(k8surl, k8suser, k8spass)
+
+        resp = self.k8s.k8s[0].delete_namespaced_secret(deleteoptions, ns, name).to_dict()
+        print json.dumps(resp, sort_keys=True, indent=2, default=self._json_serial)
+
+    def _json_serial(self, obj):
+        """JSON serializer for objects not serializable by default json code"""
+
+        if isinstance(obj, datetime):
+            serial = obj.isoformat()
+            return serial
+        raise TypeError("Type not serializable")

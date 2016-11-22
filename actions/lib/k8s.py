36e384600fce1b33cc6dd78378d629521b35b7ab
Andy Moore <andy@impulsed.net>
2016-11-22 20:16:19 +0000

    remove debugging, proper exit from function

--- actions/lib/k8s.py
+++ actions/lib/k8s.py
@@ -17,36 +17,6 @@ from datetime import datetime
 import requests
 import logging
 
-# These two lines enable debugging at httplib level (requests->urllib3->http.client)
-# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
-# The only thing missing will be the response.body which is not logged.
-#try:
-#    import http.client as http_client
-#except ImportError:
-#    # Python 2
-#    import httplib as http_client
-#http_client.HTTPConnection.debuglevel = 1
-
-# You must initialize logging, otherwise you'll not see debug output.
-#logging.basicConfig()
-#logging.getLogger().setLevel(logging.DEBUG)
-#requests_log = logging.getLogger("requests.packages.urllib3")
-#requests_log.setLevel(logging.DEBUG)
-#requests_log.propagate = True
-
-#import logging
-#logger = logging.getLogger('pyswagger')
-
-#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-#console = logging.StreamHandler()
-#console.setLevel(logging.DEBUG)
-#console.setFormatter(formatter)
-
-#logger.addHandler(console)
-#logger.setLevel(logging.DEBUG)
-
-
 class K8sClient:
 
     def __init__(self, config):
@@ -75,8 +45,7 @@ class K8sClient:
         # ctx is parsing context when producing primitives. Some primitves needs
         # multiple passes to produce(ex. Model), when we need to keep some globals
         # between passes, we should place them in ctx
-
-  
+        pass
 
     def runAction(self, action, **kwargs):
 

69d544862a75e73ddd00268dfb6481427be55911
Somogyi, Benjamin <Benjamin.Somogyi@pearson.com>
2016-11-29 12:49:48 -0700

    Update k8s TPR sensor delete action to ensure create sensors are removed.

--- actions/delete_tpr_sensor.py
+++ actions/delete_tpr_sensor.py
@@ -1,28 +1,37 @@
 from lib import k8s
 from st2actions.runners.pythonrunner import Action
 
+from jinja2 import Template
+from jinja2 import Environment, PackageLoader
+
+import os
+import json
+import jinja2
 import requests
 
 class deleteTPRSensor(Action):
 
     def run(self, payload):
 
+        allvars = {}
+
+        templateLoader = jinja2.FileSystemLoader( searchpath=self.config['template_path'] )
+        templateEnv = jinja2.Environment( loader=templateLoader , lstrip_blocks=True, trim_blocks=True)
+
         myk8s = k8s.K8sClient(self.config)
+        user = self.config['user']
+        password = self.config['password']
+        verify = self.config['verify']
 
-        k8s_api_url = self._config['kubernetes_api_url'] + "/apis/prsn.io/v1"
-        user = self._config['user']
-        password = self._config['password']
-        verify = self._config['verify']
+        tpr = payload['name']
 
-        #client = requests.get(k8s_api_url, auth=HTTPBasicAuth(user, password), verify=verify)
+        allvars['name'], allvars['domain'] = tpr.split('.', 1)
 
-        print payload
+        cname = allvars['name'].capitalize()
 
-#            'resource': ADDED
-#            'name': otherdb.prsn.io,
-#            'labels': labels,
-#            'object_kind': ThirdPartyResource,
-#            'uid': 2d17dd88-a684-11e6-aba1-02a3a04ccae9
-
-#{"type":"ADDED","object":{"kind":"ThirdPartyResource","apiVersion":"extensions/v1beta1","metadata":{"name":"otherdb.prsn.io","selfLink":"/apis/extensions/v1beta1/thirdpartyresources/otherdb.prsn.io","uid":"2d17dd88-a684-11e6-aba1-02a3a04ccae9","resourceVersion":"297758","creationTimestamp":"2016-11-09T13:55:30Z","labels":{"type":"ThirdPartyResource"}},"description":"otherdb ThirdPartyResource","versions":[{"name":"v1"}]}}
-
+        sensorpy = self.config['template_path'] +"/sensors/" + allvars['name'] + "_create.py"
+        sensorpyc = self.config['template_path'] +"/sensors/" + allvars['name'] + "_create.pyc"
+        sensoryaml = self.config['template_path'] + "/sensors/" + allvars['name'] + "_create.yaml"
+        os.remove(sensorpy)
+        os.remove(sensorpyc)
+        os.remove(sensoryaml)

a8d16d0d749b87730964b8ec5c4b03934f6ac531
Andy Moore <andy@impulsed.net>
2016-12-11 21:49:34 +0000

    testing linter

--- actions/createAppsV1beta1NamespacedStatefulSet.py
+++ actions/createAppsV1beta1NamespacedStatefulSet.py
@@ -2,6 +2,7 @@ from lib import k8s
 
 from st2actions.runners.pythonrunner import Action
 
+
 class createAppsV1beta1NamespacedStatefulSet(Action):
 
     def run(self,body,namespace,config_override=None,pretty=None):
@@ -10,16 +11,16 @@ class createAppsV1beta1NamespacedStatefulSet(Action):
 
         args = {}
         if body is not None:
-          args['body'] = body
+            args['body'] = body
         else:
-          return (False, "body is a required parameter")
+            return (False, "body is a required parameter")
         if namespace is not None:
-          args['namespace'] = namespace
+            args['namespace'] = namespace
         else:
-          return (False, "namespace is a required parameter")
+            return (False, "namespace is a required parameter")
         if config_override is not None:
-          args['config_override'] = config_override
+            args['config_override'] = config_override
         if pretty is not None:
-          args['pretty'] = pretty
+            args['pretty'] = pretty
 
         return (True, myk8s.runAction('createAppsV1beta1NamespacedStatefulSet', **args))

7fac86aaa3e0f863feba1c38bab876fdd0d466df
Andy Moore <andy@impulsed.net>
2016-12-12 19:50:47 +0000

    update for linting

--- etc/st2packgen/st2packgen.py
+++ etc/st2packgen/st2packgen.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python
 
-from jinja2 import Template
-from jinja2 import Environment, PackageLoader
-
 from pyswagger import App
 
 from shutil import copytree, copyfile
 
 import argparse
 import jinja2
-import json
 
 parser = argparse.ArgumentParser(description="Generate a kubernetes stackstorm client")
 parser.add_argument('-s', '--swagger', required=True)
@@ -42,109 +38,108 @@ copyfile(templatedir + "/sensor_template.yaml.jinja", kubedir + "/sensor_templat
 
 templateLoader = jinja2.FileSystemLoader(searchpath=templatedir)
 templateEnv = jinja2.Environment(loader=templateLoader)
-#templateEnv = jinja2.Environment(loader=templateLoader, lstrip_blocks=True, trim_blocks=True)
 
 app = App.create(swagger)
 
 for path in app.dump()['paths'].keys():
-  if not path.startswith("/api"):
-    continue
-  for method in app.dump()['paths'][path]:
-    if method is 'parameters':
-      continue
-    try:
-      operationId = app.dump()['paths'][path][method]['operationId']
-    except KeyError:
-      print "path missing operationId: %s" % path
-      continue
-    if operationId.startswith(('proxy', 'connect')):
-      continue
+    if not path.startswith("/api"):
+        continue
+    for method in app.dump()['paths'][path]:
+        if method is 'parameters':
+            continue
+        try:
+            operationId = app.dump()['paths'][path][method]['operationId']
+        except KeyError:
+            print "path missing operationId: %s" % path
+            continue
+        if operationId.startswith(('proxy', 'connect')):
+            continue
 
-    print "creating path %s method %s" % (operationId, method)
+        print "creating path %s method %s" % (operationId, method)
 
-    op = app.op[operationId]
+        op = app.op[operationId]
 
-    allvars = {}
-    allvars['swaggerspec'] = swagger
+        allvars = {}
+        allvars['swaggerspec'] = swagger
 
-    allvars['paramsreq'] = []
-    allvars['params'] = []
+        allvars['paramsreq'] = []
+        allvars['params'] = []
 
-    tmp = {}
-    tmp['type'] = 'object'
-    tmp['description'] = 'override stackstorm config'
-    tmp['required'] = False
-    tmp['name'] = 'config_override'
-
-    allvars['params'].append(tmp)
-
-    allvars['path'] = op.path
-    allvars['operationId'] = op.operationId
-    allvars['description'] = op.description
-    allvars['method'] = method
-
-    for x in op.parameters:
-      tmp = {}
-      if x.schema is not None:
+        tmp = {}
         tmp['type'] = 'object'
-        ref = getattr(x.schema, '$ref')
-        refdata = app.resolve(ref)
-        if refdata.description is not None:
-          tmp['description'] = refdata.description.replace('"','')
-        else:
-          tmp['description'] = ""
-      else:
-        tmp['type'] = x.type
-      if x.description is not None:
-        tmp['description'] = x.description.replace('"', '')
-      else:
-        tmp['description'] = ""
-      tmp['required'] = x.required
-      tmp['name'] = x.name
-      tmp['default'] = x.default
-      tmp['pattern'] = x.pattern
-      tmp['in'] = getattr(x, 'in')
-      if tmp['required'] == True:
-        allvars['paramsreq'].append(tmp)
-      else:
+        tmp['description'] = 'override stackstorm config'
+        tmp['required'] = False
+        tmp['name'] = 'config_override'
+
         allvars['params'].append(tmp)
 
-    if operationId.startswith(('watch')):
-      reqcount = len(allvars['paramsreq'])
-      if reqcount == 0:
-        thename = allvars['path'].split('/')[-1]
+        allvars['path'] = op.path
+        allvars['operationId'] = op.operationId
+        allvars['description'] = op.description
+        allvars['method'] = method
 
-        allvars['kind'] = thename.capitalize()
-        allvars['watchurl'] = allvars['path']
-        allvars['name'] = thename
-        allvars['triggername'] = thename
+        for x in op.parameters:
+            tmp = {}
+            if x.schema is not None:
+                tmp['type'] = 'object'
+                ref = getattr(x.schema, '$ref')
+                refdata = app.resolve(ref)
+                if refdata.description is not None:
+                    tmp['description'] = refdata.description.replace('"', '')
+                else:
+                    tmp['description'] = ""
+            else:
+                tmp['type'] = x.type
+            if x.description is not None:
+                tmp['description'] = x.description.replace('"', '')
+            else:
+                tmp['description'] = ""
+            tmp['required'] = x.required
+            tmp['name'] = x.name
+            tmp['default'] = x.default
+            tmp['pattern'] = x.pattern
+            tmp['in'] = getattr(x, 'in')
+            if tmp['required'] == True:
+                allvars['paramsreq'].append(tmp)
+            else:
+                allvars['params'].append(tmp)
 
-        sensorpy = sensorsdir + "/" + allvars['name'] + ".py"
-        sensoryaml = sensorsdir + "/" + allvars['name'] + ".yaml"
-        p = open(sensorpy, 'w')
-        y = open(sensoryaml, 'w')
+        if operationId.startswith(('watch')):
+            reqcount = len(allvars['paramsreq'])
+            if reqcount == 0:
+                thename = allvars['path'].split('/')[-1]
 
-        template = templateEnv.get_template('sensor_template.py.jinja')
-        outputText = template.render(allvars)
-        p.write(outputText)
-        template = templateEnv.get_template('sensor_template.yaml.jinja')
-        outputText = template.render(allvars)
-        y.write(outputText)
+                allvars['kind'] = thename.capitalize()
+                allvars['watchurl'] = allvars['path']
+                allvars['name'] = thename
+                allvars['triggername'] = thename
 
-        p.close()
-        y.close()
-    else:
-      actionpy = actionsdir + "/" + allvars['operationId'] + ".py"
-      actionyaml = actionsdir + "/" + allvars['operationId'] + ".yaml"
-      p = open(actionpy, 'w')
-      y = open(actionyaml, 'w')
+                sensorpy = sensorsdir + "/" + allvars['name'] + ".py"
+                sensoryaml = sensorsdir + "/" + allvars['name'] + ".yaml"
+                p = open(sensorpy, 'w')
+                y = open(sensoryaml, 'w')
 
-      template = templateEnv.get_template('action_template.py.jinja')
-      outputText = template.render(allvars)
-      p.write(outputText)
-      template = templateEnv.get_template('action_template.yaml.jinja')
-      outputText = template.render(allvars)
-      y.write(outputText)
+                template = templateEnv.get_template('sensor_template.py.jinja')
+                outputText = template.render(allvars)
+                p.write(outputText)
+                template = templateEnv.get_template('sensor_template.yaml.jinja')
+                outputText = template.render(allvars)
+                y.write(outputText)
 
-      p.close()
-      y.close()
+                p.close()
+                y.close()
+        else:
+            actionpy = actionsdir + "/" + allvars['operationId'] + ".py"
+            actionyaml = actionsdir + "/" + allvars['operationId'] + ".yaml"
+            p = open(actionpy, 'w')
+            y = open(actionyaml, 'w')
+
+            template = templateEnv.get_template('action_template.py.jinja')
+            outputText = template.render(allvars)
+            p.write(outputText)
+            template = templateEnv.get_template('action_template.yaml.jinja')
+            outputText = template.render(allvars)
+            y.write(outputText)
+
+            p.close()
+            y.close()

d12bc95acf86dfdbbae5cdd7fade56e55f51c6a8
Andy Moore <andy@impulsed.net>
2016-12-13 12:40:00 +0000

    split the lines to appease the linters

--- etc/st2packgen/st2packgen.py
+++ etc/st2packgen/st2packgen.py
@@ -85,7 +85,7 @@ for path in app.dump()['paths'].keys():
                 ref = getattr(x.schema, '$ref')
                 refdata = app.resolve(ref)
                 if refdata.description is not None:  # pylint: disable=no-member
-                    tmp['description'] = 
+                    tmp['description'] = \  # pylint: disable=no-member
                         refdata.description.replace('"', '')  # pylint: disable=no-member
                 else:
                     tmp['description'] = ""

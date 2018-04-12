9179b601a2ab49b858644df9be997e5938cbb7a0
Andy Moore <andy@impulsed.net>
2018-04-12 23:41:26 +0100

    tidy for linting

--- actions/register_sensor.py
+++ actions/register_sensor.py
@@ -3,12 +3,11 @@
 from __future__ import absolute_import
 
 import st2common
-from st2common import config
 import st2common.script_setup
-
 import st2common.bootstrap.sensorsregistrar as sensors_registrar
 
 from st2common.runners.base_action import Action
+
 
 class RegisterSensor(Action):
 
@@ -21,7 +20,7 @@ class RegisterSensor(Action):
         st2common.script_setup.register_exchanges_with_retry()
         st2common.script_setup.db_setup()
         st2common.triggers.register_internal_trigger_types()
-        
+
         registrar = sensors_registrar.SensorsRegistrar(use_pack_cache=True, fail_on_failure=True)
 
         print registrar._register_sensor_from_pack("kubernetes", destfile)

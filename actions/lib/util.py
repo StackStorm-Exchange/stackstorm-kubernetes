4edbd209ee207efd491418001f5e9ec1e05fbfc2
Gamunu Balagalla <udaya.balagalla@pearson.com>
2018-12-16 21:14:30 +0530

    Add built in types

--- actions/lib/util.py
+++ actions/lib/util.py
@@ -7,6 +7,8 @@ def json_serial(obj):
     if isinstance(obj, (datetime, date)):
         serial = obj.isoformat()
         return serial
-    if isinstance(obj, object):
+    elif isinstance(obj, (int, long, float, complex, bool, str, unicode, basestring)):
+        return obj
+    elif isinstance(obj, object):
         return {k.lstrip('_'): v for k, v in vars(obj).items()}
     raise TypeError("Type %s not serializable" % type(obj))
